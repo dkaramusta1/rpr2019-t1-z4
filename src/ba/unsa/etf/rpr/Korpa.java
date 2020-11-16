81df2d3c945faf307fe6e9544b3122735a0f9070
Dzana <dkaramusta1@etf.unsa.ba>
2020-11-16 16:47:17 +0100

    Adds class atributes and methods so the main function is working

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private static int index = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public boolean dodajArtikl(Artikl artikl) {
+        if(index < artikli.length) {
+            this.artikli[index] = artikl;
+            index = index + 1;
+            return true;
+        }
+        return  false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl izbaceni = null;
+        for(int i = 0; i < index; i++){
+            if(this.artikli[i].getKod().equals(kod)){
+                String naziv = this.artikli[i].getNaziv();
+                int cijena = this.artikli[i].getCijena();
+                String code = this.artikli[i].getKod();
+                izbaceni = new Artikl(naziv,cijena,code);
+                this.artikli[i] = null;
+                index = index - 1;
+                break;
+            }
+        }
+        return izbaceni;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int total = 0;
+        for(int i = 0; i < index; i++){
+            total = total + artikli[i].getCijena();
+        }
+        return total;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private static int index = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public void setArtikli(Artikl[] artikli) {
+        this.artikli = artikli;
+    }
+
+    public void dodajArtikl(Artikl artikl) {
+        this.artikli[index] = artikl;
+        index = index + 1;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl izbaceni = null;
+        for(int i = 0; i < index; i++){
+            if(this.artikli[i].getKod().equals(kod)){
+                String naziv = this.artikli[i].getNaziv();
+                int cijena = this.artikli[i].getCijena();
+                String code = this.artikli[i].getKod();
+                izbaceni = new Artikl(naziv,cijena,code);
+                this.artikli[i] = null;
+                index = index - 1;
+                break;
+            }
+        }
+        return izbaceni;
+    }
 }
